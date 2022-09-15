fffa348b739cb371b4ef1c12b7397e29bcfc6855
Tosin Jenkeo <tjenkeo1234@gmail.com>
2022-09-15 15:13:23 +0100

    TJ: added repo and service packages to API

---
+++ src/main/java/com/bae/main/services/ClubService.java
@@ -0,0 +1,61 @@
+package com.bae.main.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.bae.main.entities.Club;
+import com.bae.main.repos.ClubRepo;
+
+@Service
+public class ClubService {
+
+private ClubRepo repo;
+
+
+
+public ClubService(ClubRepo repo) {
+	super();
+	this.repo = repo;
+}
+
+
+public Club create(Club input) {
+	return repo.saveAndFlush(input);
+}
+
+
+public List<Club> getAll() {
+	return repo.findAll();
+}
+
+
+public Club getById(long id) {
+	return repo.findById(id).get();
+}
+
+public Club getByClubName(String clubName) {
+	return repo.findClubByClubName(clubName);
+	}
+
+public Club update(long id, Club input) {
+	Club existing = repo.findById(id).get();
+	
+	existing.setClubName(input.getClubName());
+	existing.setClubNationality(input.getClubNationality());
+	existing.setClubTitles(input.getClubTitles());
+	
+	return repo.saveAndFlush(existing);
+	
+}
+
+
+
+public boolean delete(long id) {
+	repo.deleteById(id);
+	
+	return !repo.existsById(id);
+}
+
+}
